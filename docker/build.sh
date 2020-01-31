df5c78bad126b0887221e24352a7f63ac2beaedb
murumkar <manoj.murumkar@gmail.com>
2020-01-31 10:44:35 -0800

    Fixed build.sh to create directories, if they don't exist

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+mkdir -p cp-kafka-oauth/libs
+mkdir -p rest-proxy/libs
+
 rm rest-proxy/libs/kafka*
 rm cp-kafka-oauth/libs/kafka*
+
 cp ../kafka-ims-rest/target/*-jar-with-dependencies.jar ./rest-proxy/libs/
 cp ../kafka-ims-java/target/*-jar-with-dependencies.jar ./cp-kafka-oauth/libs/
 
